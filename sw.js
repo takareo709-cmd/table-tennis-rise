e414aef0a3850cd122f40ee196ef5690dd7cbd51
takareo709-cmd <takareo709@gmail.com>
2026-08-13 11:26:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="tt-rise-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) );
